9931326b4d69bdd1c07c244723419b37a31411ad
kokubo <t.kokubo.sys24@morijyobi.ac.jp>
2024-11-05 10:57:24 +0900

    Aの作業

--- janken.py
+++ janken.py
@@ -1,1 +1,13 @@
+you_win = 0
+cpu_win = 0
+while you_win + cpu_win < 3:
+    
+    
 
+
+print("【最終結果】")
+print(f'あなた：{you_win}勝 \nコンピューター：{cpu_win}勝')
+if you_win > cpu_win:
+    print("お前の勝ち❣")
+elif you_win < cpu_win:
+    print("お前の負け❣")
