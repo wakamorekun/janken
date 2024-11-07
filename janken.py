45bb8ec8570f56bb23091829be1871b0cab14510
arata <a.kuse.sys24@morijyobi.ac.jp>
2024-11-07 11:46:47 +0900

    cpuの乱数生成#4

--- janken.py
+++ janken.py
@@ -1,4 +1,5 @@
-
+import random
+cpu = random.randint(1, 3)
 janken = int(input(f"1.グー\n2.チョキ\n3.パー\nあなたの手を選択してください。>"))
 
 
@@ -12,6 +13,3 @@ elif(janken == 1 and cpu == 2) or (janken == 2 and cpu == 3) or (janken == 3 and
 else:
     print("<><><><><><><><><><><>\nコンピュータの勝ちです！\n<><><><><><><><><><><>")
     cpu_win += 1
-    
-
-
